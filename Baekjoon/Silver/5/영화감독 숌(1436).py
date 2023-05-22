f8b017462ddfc8da721f5ce348b486821456fd30
18-12847 <118495919+18-12847@users.noreply.github.com>
2023-05-22 23:33:47 +0900

    실버5 소스 코드

---
+++ Baekjoon/Silver/5/영화감독 숌(1436).py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+cnt = 0
+for i in range(666, 1000000000):
+    if "666" in str(i):
+        cnt += 1
+    if cnt == n:
+        print(i)
+        break
