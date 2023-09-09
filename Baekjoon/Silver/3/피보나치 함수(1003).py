153c6fd3f9540f410f7ac349932d6b2defb6aa49
SeungJae Oh <118495919+18-12847@users.noreply.github.com>
2023-09-09 17:19:12 +0900

    실버3 소스 코드

---
+++ Baekjoon/Silver/3/피보나치 함수(1003).py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+for _ in range(int(input().rstrip())):
+    n = int(input().rstrip())
+    dp = [[0, 0] for _ in range(n + 2)]
+    dp[0], dp[1] = [1, 0], [0, 1]
+    for i in range(2, n + 1):
+        dp[i][0], dp[i][1] = dp[i-2][0] + dp[i-1][0], dp[i-2][1] + dp[i-1][1]
+    print(*dp[n])
