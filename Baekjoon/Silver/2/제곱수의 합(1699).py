dd1435d004141527fcab12f8485b3830e2d1da63
SeungJae Oh <118495919+18-12847@users.noreply.github.com>
2023-10-08 22:02:47 +0900

    실버2 소스 코드

---
+++ Baekjoon/Silver/2/제곱수의 합(1699).py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input().rstrip())
+dp = [i for i in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, int(i ** 0.5) + 1):
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+print(dp[n])
