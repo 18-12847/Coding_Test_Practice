cfa77b6c94b4527773fe6e6911ec164d54999f4f
SeungJae Oh <118495919+18-12847@users.noreply.github.com>
2023-11-12 22:56:06 +0900

    실버1 소스 코드

---
+++ Baekjoon/Silver/1/본대 산책(12849).py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+dp = [1] + [0] * 7
+for _ in range(int(input().rstrip())):
+    tmp = [0] * 8
+    tmp[0] = dp[1] + dp[2]
+    tmp[1] = dp[0] + dp[2] + dp[3]
+    tmp[2] = dp[0] + dp[1] + dp[3] + dp[4]
+    tmp[3] = dp[1] + dp[2] + dp[4] + dp[5]
+    tmp[4] = dp[2] + dp[3] + dp[5] + dp[7]
+    tmp[5] = dp[3] + dp[4] + dp[6]
+    tmp[6] = dp[5] + dp[7]
+    tmp[7] = dp[4] + dp[6]
+    for i in range(8):
+        dp[i] = tmp[i] % 1000000007
+print(dp[0])
